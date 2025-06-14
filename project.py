b7676111ba06d8c330854a4fe9d2daf8a793393e
Chelikani Ramakrishna <ramakrishnachelikani03@gmail.com>
2025-06-14 12:06:43 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,42 @@
+{  " averageSockprice": 453.569744,
+"priceHistory": [
+    "averagestockprice":453.569744,
+    "lastUpdateAt": "2025-05-08T04:26:27.465842874z"
+},
+{
+    "price": 231.95156,
+    "lastUpdateAt": "2025-05-08T04":30:23.465842874Z";
+},
+{
+    "price":459.09558,"
+    "lastUpdateAt": "2025-05-08T04":30:23.465842874Z";,
+
+},
+{
+    "price":459.09558,"
+    "lastUpdateAt": "2025-05-08T04":30:23.465842874Z";,
+
+}
+{
+    "correlation": -0.9367,
+    "stocks":{
+        "NVDA": {
+            "averagePrice": 204.00025,
+            "priceHistory": [
+                {
+                    "price":459.09558,"
+      "lastUpdateAt": "2025-05-08T04":30:23.465842874Z";,
+                },
+                
+                    "price":459.09558,"
+      "lastUpdateAt": "2025-05-08T04":30:23.465842874Z";,
+                },
+                 
+                    "price":459.09558,"
+      "lastUpdateAt": "2025-05-08T04":30:23.465842874Z";,
+                },  
+                }
+            ]
+        }
+    }
+}
