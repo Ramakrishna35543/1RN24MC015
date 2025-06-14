5d046e8cde79206b8cb4d3cd630d8f02d3421a61
Chelikani Ramakrishna <ramakrishnachelikani03@gmail.com>
2025-06-14 12:07:12 +0530

    Add files via upload

---
+++ register.py
@@ -0,0 +1,9 @@
+{
+    "email": "chelikaniramakrishna24mca@rnsit.ac.in,
+    "name":"Rama krishna",
+    "mobile no": "1RN24MC015",
+    "githubUsername":"github"
+    "collegeName":"Rnsit University",
+    "accessCode":"pmVsEh"
+}
+
